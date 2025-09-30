4baa8cea6f4c43b7cf3fef268d11044c98841da5
gokulpavan-999 <pavanthota480@gmail.com>
2025-09-30 15:28:58 +0530

    Update cart.sh

--- cart.sh
+++ cart.sh
@@ -6,6 +6,13 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOGS_FOLDER="/var/log/Shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+
 if [ $USERID -ne 0 ]; then
   echo "Please run this Script with root Privilege"
   exit 1
@@ -13,20 +20,20 @@ fi
 
 VALIDATE(){
    if [ $1 -ne 0 ]; then
-     echo -e "$2 ... $R FAILURE $N"
+     echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
    else
-     echo -e "$2 ... $G SUCCESS $N"
+     echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
    fi
 }
 
 ####Nodejs####
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOG_FILE
 VALIDATE $? "Disabled Default Nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$LOG_FILE
 VALIDATE $? "Enabling Nodejs"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing Nodejs"
 
 id roboshop
@@ -36,29 +43,29 @@ id roboshop
     echo -e "User already exist ... $Y SKIPPING $N"
   fi
 
-  mkdir -p /app
+  mkdir -p /app &>>$LOG_FILE
   VALIDATE $? "Create app directory"
 
-  curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+  curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOG_FILE
   VALIDATE $? "Download cart Application"
 
-  cd /app
+  cd /app &>>$LOG_FILE
   VALIDATE $? "Changing to app directory"
 
-  rm -rf /app/*
+  rm -rf /app/* &>>$LOG_FILE
   VALIDATE $? "Remove existing code"
 
-  unzip /tmp/cart.zip
+  unzip /tmp/cart.zip &>>$LOG_FILE
   VALIDATE $? "Unzip cart"
 
-  npm install
+  npm install &>>$LOG_FILE
   VALIDATE $? "Installing Dependencies"
 
   cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
   VALIDATE $? "Copy systemctl service"
 
   systemctl daemon-reload
-  systemctl enable cart
+  systemctl enable cart &>>$LOG_FILE
   VALIDATE $? "Enabling cart"
 
   systemctl restart cart
