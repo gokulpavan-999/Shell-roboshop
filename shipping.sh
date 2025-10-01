1acfe272ea974422c83fdfc67833fa10b2e84043
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-01 16:11:34 +0530

    Update shipping.sh

--- shipping.sh
+++ shipping.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 USERID=$(id -u)
@@ -6,75 +7,72 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/Shell-roboshop"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-MYSQL_HOST=mysql.pavandevops.fun
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+MYSQL_HOST=mysql.pavandevops.fun
 
 mkdir -p $LOGS_FOLDER
-echo -e "Script started executed at: $(date)" | tee -a $LOG_FILE
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-   echo "Please run this Script with root Privilege"
-   exit 1
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE(){
-   if [ $1 -ne 0 ]; then
-     echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
-   else
-     echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
-  fi
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
 }
 
 dnf install maven -y &>>$LOG_FILE
-VALIDATE $? "Installing Maven"
 
 id roboshop &>>$LOG_FILE
-  if [ $? -ne 0 ]; then
-     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-     VALIDATE $? "Creating User"
-  else
-    echo -e "User already exists ... $Y SKIPPING $N"
-  fi
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "User already exist ... $Y SKIPPING $N"
+fi
 
-mkdir -p /app &>>$LOG_FILE
-VALIDATE $? "Create app directory"
-  
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
-VALIDATE $? "Download shipping Application"
+mkdir -p /app
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading shipping application"
 
 cd /app 
-VALIDATE $? "Change to app directory"
+VALIDATE $? "Changing to app directory"
 
-rm -rf /app/* &>>$LOG_FILE
-VALIDATE $? "Remove existing code"
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
 
 unzip /tmp/shipping.zip &>>$LOG_FILE
-VALIDATE $? "Unzip shipping"
+VALIDATE $? "unzip shipping"
 
-mvn clean package &>>$LOG_FILE
+mvn clean package  &>>$LOG_FILE
 mv target/shipping-1.0.jar shipping.jar 
 
-cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>$LOG_FILE
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
 systemctl daemon-reload
-systemctl enable shipping &>>$LOG_FILE
-VALIDATE $? "Enabling shipping"
+systemctl enable shipping  &>>$LOG_FILE
 
-dnf install mysql -y &>>$LOG_FILE
-VALIDATE $? "Installing MYSQL"
+dnf install mysql -y  &>>$LOG_FILE
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
-  if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]; then
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
-  else
-    echo -e "Shipping Data is already loaded ... $Y SKIPPING $N"
-  fi
+else
+    echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
+fi
 
-systemctl restart shipping 
-VALIDATE $? "Restarted Shipping"
-
+systemctl restart shipping
   
