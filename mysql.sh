61bd04dfd6bb21a4e466adb03d4f5d25a4bf5b98
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-16 12:28:36 +0530

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -6,48 +6,37 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/Shell-roboshop"
+LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
-
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-   echo "ERROR:: Please run this Script with root privilege"
-   exit 1
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE(){
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-       echo -e "$2 ... $R FAILURE $ N" | tee -a $LOG_FILE
-       exit 1
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
     else
-       echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
 dnf install mysql-server -y &>>$LOG_FILE
-VALIDATE $? "Installing MYSQL Server"
-
+VALIDATE $? "Installing MySQL Server"
 systemctl enable mysqld &>>$LOG_FILE
-VALIDATE $? "Enabling MYSQL Server"
+VALIDATE $? "Enabling MySQL Server"
+systemctl start mysqld   &>>$LOG_FILE
+VALIDATE $? "Starting MySQL Server"
 
-systemctl start mysqld &>>$LOG_FILE
-VALIDATE $? "Starting MYSQL Server"
-
-#mysql_secure_installation --set-root-pass Roboshop@1 &>>$LOG_FILE
-#VALIDATE $? "Setting up root password"
-
-mysql -uroot -pRoboShop@1 -e "show databases;" &>>$LOG_FILE
-if [ $? -ne 0 ]; then
-  mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
-  VALIDATE $? "Setting up root password"
-else
-  echo -e "Root password already set ... ${Y}SKIPPING${N}"
-fi
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+VALIDATE $? "Setting up Root password"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-echo -e "Script expected in: $Y $TOTAL_TIME seconds $N"
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
